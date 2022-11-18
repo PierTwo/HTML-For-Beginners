dace8885a2e86945214359ad7e1873e312a52fd5
Jordan <jordan.b3360@gmail.com>
2022-11-17 20:35:52 -0600

    added client side, with boilerplate and connected apollo/gql, may need to modify login, and signup and or mutations etc..

---
+++ client/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { useQuery } from "@apollo/client";
+
+const Home = () => {
+  return (
+    <main>
+      <div className="flex-row justify-center">
+        <h3>This is test, the boilerplate is working</h3>
+      </div>
+    </main>
+  );
+};
+
+export default Home;
