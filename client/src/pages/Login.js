92ebe6a492cb99a092fc6bb51a6911de171bd518
Jordan <jordan.b3360@gmail.com>
2022-11-17 20:50:01 -0600

    changed className in client since we will most likely be using Tailwinds CSS instead of bootstraps

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,12 +1,12 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { useMutation } from '@apollo/client';
-import { LOGIN_USER } from '../utils/mutations';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { useMutation } from "@apollo/client";
+import { LOGIN_USER } from "../utils/mutations";
 
-import Auth from '../utils/auth';
+import Auth from "../utils/auth";
 
 const Login = (props) => {
-  const [formState, setFormState] = useState({ email: '', password: '' });
+  const [formState, setFormState] = useState({ email: "", password: "" });
   const [login, { error, data }] = useMutation(LOGIN_USER);
 
   // update state based on form input changes
@@ -35,20 +35,20 @@ const Login = (props) => {
 
     // clear form values
     setFormState({
-      email: '',
-      password: '',
+      email: "",
+      password: "",
     });
   };
 
   return (
-    <main className="flex-row justify-center mb-4">
-      <div className="col-12 col-lg-10">
+    <main className="">
+      <div className="">
         <div className="card">
-          <h4 className="card-header bg-dark text-light p-2">Login</h4>
+          <h4 className="card-header">Login</h4>
           <div className="card-body">
             {data ? (
               <p>
-                Success! You may now head{' '}
+                Success! You may now head{" "}
                 <Link to="/">back to the homepage.</Link>
               </p>
             ) : (
@@ -70,8 +70,8 @@ const Login = (props) => {
                   onChange={handleChange}
                 />
                 <button
-                  className="btn btn-block btn-primary"
-                  style={{ cursor: 'pointer' }}
+                  className="btn"
+                  style={{ cursor: "pointer" }}
                   type="submit"
                 >
                   Submit
@@ -79,11 +79,7 @@ const Login = (props) => {
               </form>
             )}
 
-            {error && (
-              <div className="my-3 p-3 bg-danger text-white">
-                {error.message}
-              </div>
-            )}
+            {error && <div className="">{error.message}</div>}
           </div>
         </div>
       </div>
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -34,16 +34,13 @@ const Profile = () => {
 
   return (
     <div>
-      <div className="flex-row justify-center mb-3">
-        <h2 className="col-12 col-md-10 bg-dark text-light p-3 mb-5">
+      <div className="">
+        <h2 className="">
           Viewing {userParam ? `${user.username}'s` : "your"} profile.
         </h2>
 
         {!userParam && (
-          <div
-            className="col-12 col-md-10 mb-3 p-3"
-            style={{ border: "1px dotted #1a1a1a" }}
-          ></div>
+          <div className="" style={{ border: "1px dotted #1a1a1a" }}></div>
         )}
       </div>
     </div>
