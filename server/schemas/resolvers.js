4359e224524564523a47c811bbc826b50210c763
PierTwo <105090994+PierTwo@users.noreply.github.com>
2022-11-21 22:07:52 -0600

    Prettier and lint configured

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,44 +1,44 @@
-const { AuthenticationError } = require("apollo-server-express");
-const { User } = require("../models");
-const { signToken } = require("../utils/auth");
+const { AuthenticationError } = require('apollo-server-express')
+const { User } = require('../models')
+const { signToken } = require('../utils/auth')
 
 const resolvers = {
   Query: {
     user: async (parent, { username }) => {
-      return User.findOne({ username });
+      return User.findOne({ username })
     },
     me: async (parent, args, context) => {
       if (context.user) {
-        return User.findOne({ _id: context.user._id });
+        return User.findOne({ _id: context.user._id })
       }
-      throw new AuthenticationError("You need to be logged in!");
-    },
+      throw new AuthenticationError('You need to be logged in!')
+    }
   },
 
   Mutation: {
     addUser: async (parent, { username, email, password }) => {
-      const user = await User.create({ username, email, password });
-      const token = signToken(user);
-      return { token, user };
+      const user = await User.create({ username, email, password })
+      const token = signToken(user)
+      return { token, user }
     },
     login: async (parent, { email, password }) => {
-      const user = await User.findOne({ email });
+      const user = await User.findOne({ email })
 
       if (!user) {
-        throw new AuthenticationError("No user found with this email address");
+        throw new AuthenticationError('No user found with this email address')
       }
 
-      const correctPw = await user.isCorrectPassword(password);
+      const correctPw = await user.isCorrectPassword(password)
 
       if (!correctPw) {
-        throw new AuthenticationError("Incorrect credentials");
+        throw new AuthenticationError('Incorrect credentials')
       }
 
-      const token = signToken(user);
+      const token = signToken(user)
 
-      return { token, user };
-    },
-  },
-};
+      return { token, user }
+    }
+  }
+}
 
-module.exports = resolvers;
+module.exports = resolvers
