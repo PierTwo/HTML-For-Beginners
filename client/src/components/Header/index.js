7496116d2e6931518b423520cdbef4460fa27769
Jordan <jordan.b3360@gmail.com>
2022-11-21 21:56:52 -0600

    made the navbar/header have more styling

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -9,32 +9,35 @@ const Header = () => {
     Auth.logout();
   };
   return (
-    <header className="bg-gray-200 container mx-auto px-4">
+    <header className="bg-neutral-200 container mx-auto px-4">
       <div className="flex justify-start">
         <div>
           <Link className="text-white" to="/">
-            <h1 className="text-2xl bg-sky-500/100 px-4 pt-2 pb-2 mb-4 rounded-b-lg">
+            <h1 className="font-extrabold text-3xl bg-sky-500/100 px-4 pt-2 pb-2 mb-4 rounded-b-lg">
               Tutorials
             </h1>
           </Link>
-          <p className="m-0">HTML Tutorials For Beginners... </p>
+          <h2 className="m-2 text-lg">HTML Tutorials For Beginners... </h2>
         </div>
         <div className="">
           {Auth.loggedIn() ? (
             <>
-              <Link className="btn bg-sky-400 p-3 m-2" to="/me">
+              <Link className="btn font-medium p-3 m-2" to="/me">
                 {Auth.getProfile().data.username}'s profile
               </Link>
-              <button className="btn bg-sky-400 p-2 m-2" onClick={logout}>
+              <button
+                className="btn rounded font-medium bg-sky-400 p-2 m-2"
+                onClick={logout}
+              >
                 Logout
               </button>
             </>
           ) : (
             <div className="mt-2">
-              <Link className="btn bg-sky-400 p-3" to="/login">
+              <Link className="btn font-medium p-3" to="/login">
                 Login
               </Link>
-              <Link className="btn bg-sky-400 p-3" to="/signup">
+              <Link className="btn font-medium p-3" to="/signup">
                 Signup
               </Link>
             </div>
