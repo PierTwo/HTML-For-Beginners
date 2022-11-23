fa87b99efdf7d2f0af4c66785f90a6b23b9e7a3c
Jordan <jordan.b3360@gmail.com>
2022-11-22 20:41:19 -0600

    added styling to the quiz, still needs some refining

--- client/src/pages/Quiz.js
+++ client/src/pages/Quiz.js
@@ -86,48 +86,52 @@ const Quiz = () => {
     setShowResults(false);
   };
   return (
-    <div>
-      {/* 1. Header  */}
-      <h1>HTML Quiz</h1>
+    <div className="mt-8 flex justify-center">
+      <div className="grid justify-items-center bg-sky-200">
+        {/* 1. Header  */}
+        <div className="pt-2 pb-2 mt-2 mb-2">
+          <h1 className="font-extrabold text-3xl">HTML Quiz</h1>
 
-      {/* 2. Current Score  */}
-      <h2>Score: {score}</h2>
-
-      {/* 3. Show results or show the question game  */}
-      {showResults ? (
-        /* 4. Final Results */
-        <div className="final-results">
-          <h1>Final Results</h1>
-          <h2>
-            {score} out of {questions.length} correct - (
-            {(score / questions.length) * 100}%)
-          </h2>
-          <button onClick={() => restartGame()}>Restart game</button>
+          {/* 2. Current Score  */}
+          <h2 className="font-bold text-xl">Score: {score}</h2>
         </div>
-      ) : (
-        /* 5. Question Card  */
-        <div className="question-card">
-          {/* Current Question  */}
-          <h2>
-            Question: {currentQuestion + 1} out of {questions.length}
-          </h2>
-          <h3 className="question-text">{questions[currentQuestion].text}</h3>
 
-          {/* List of possible answers  */}
-          <ul>
-            {questions[currentQuestion].options.map((option) => {
-              return (
-                <li
-                  key={option.id}
-                  onClick={() => optionClicked(option.isCorrect)}
-                >
-                  {option.text}
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-      )}
+        {/* 3. Show results or show the question game  */}
+        {showResults ? (
+          /* 4. Final Results */
+          <div className="pt-2 pb-2 mt-2 mb-2">
+            <h1>Final Results</h1>
+            <h2>
+              {score} out of {questions.length} correct - (
+              {(score / questions.length) * 100}%)
+            </h2>
+            <button onClick={() => restartGame()}>Restart game</button>
+          </div>
+        ) : (
+          /* 5. Question Card  */
+          <div className="p-2 m-2">
+            {/* Current Question  */}
+            <h2 className="mt-2 mb-2">
+              Question: {currentQuestion + 1} out of {questions.length}
+            </h2>
+            <h3 className="mt-3 mb-3">{questions[currentQuestion].text}</h3>
+
+            {/* List of possible answers  */}
+            <ul>
+              {questions[currentQuestion].options.map((option) => {
+                return (
+                  <li
+                    key={option.id}
+                    onClick={() => optionClicked(option.isCorrect)}
+                  >
+                    {option.text}
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
